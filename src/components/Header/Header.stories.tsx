b63222101861515f4aa56172a67fa28ceb42b149
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-11 15:22:48 -0300

    feat: add header component

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Header } from "./Header";
+
+const meta: Meta<typeof Header> = { component: Header };
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = { render: Header };
