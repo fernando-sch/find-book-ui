89b0e1bfa2660209c000f5b11f72ed29c78350f6
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-11 18:34:30 -0300

    chore: update button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,29 @@
+import classNames from "classnames";
+
 type ButtonProps = {
   title: string;
+  onClick: () => void;
+  variant?: "dark" | "light";
+  className?: string;
 };
 
-export const Button = ({ title }: ButtonProps) => {
+export const Button = ({
+  title,
+  onClick,
+  variant = "dark",
+  className = "",
+}: ButtonProps) => {
   return (
-    <button className="bg-evergreen-light px-3 py-2 rounded-lg shadow text-whiteish font-medium">
+    <button
+      className={classNames([
+        "px-3 py-2 rounded-lg shadow font-medium",
+        variant === "dark" && "bg-evergreen-light text-white",
+        variant === "light" &&
+          "bg-white border-2 border-evergreen-light text-evergreen-light",
+        className,
+      ])}
+      onClick={onClick}
+    >
       {title}
     </button>
   );
