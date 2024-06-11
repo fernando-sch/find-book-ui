c1cfcb09de73fd15100653491c3b6036b1fa626c
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-11 13:02:13 -0300

    chore: change postcss and tailwind config file to be .js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        evergreen: "#2b4341",
+      },
+    },
   },
   plugins: [],
-}
-
+};
