380a70583738bdad49a0b8aa4d99597c764a26c6
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-11 17:35:34 -0300

    feat: add dashboard page

---
+++ src/pages/Dashboard/Dashboard.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Dashboard } from "./Dashboard";
+
+const meta: Meta<typeof Dashboard> = { component: Dashboard };
+export default meta;
+
+type Story = StoryObj<typeof Dashboard>;
+
+export const Default: Story = { render: Dashboard };
