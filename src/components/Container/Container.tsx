830d8c2e4d746a454145bb9ad98ea1f059fbeb5f
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-11 17:35:25 -0300

    chore: update container component

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 type ContainerProps = {
   children: React.ReactNode;
+  tailStyles?: string;
 };
 
-export const Container = ({ children }: ContainerProps) => {
-  return <div className="px-2 md:px-5 py-4 max-w-7xl mx-auto w-full">{children}</div>;
+export const Container = ({ children, tailStyles = "" }: ContainerProps) => {
+  const baseTailStyled = "px-2 md:px-5 py-4 max-w-7xl mx-auto w-full";
+  const className = tailStyles
+    ? `${baseTailStyled} ${tailStyles}`
+    : baseTailStyled;
+
+  return <div className={className}>{children}</div>;
 };
