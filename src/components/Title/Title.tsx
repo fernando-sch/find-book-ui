17f1ce802d05982d61f0b00e5265508e433593aa
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-12 15:54:28 -0300

    chore: update title component

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,10 +1,18 @@
+import classNames from "classnames";
+
 type TitleProps = {
   title: string;
+  className?: string;
 };
 
-export const Title = ({ title }: TitleProps) => {
+export const Title = ({ title, className = "" }: TitleProps) => {
   return (
-    <h6 className="text-5xl border-b-evergreen-light border-b-4 w-fit font-bold text-evergreen">
+    <h6
+      className={classNames(
+        "md:text-5xl text-2xl border-b-evergreen-light border-b-4 w-fit font-bold text-evergreen",
+        className
+      )}
+    >
       {title}
     </h6>
   );
