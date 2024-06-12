acf62278cb2066f2ff89658e8495abc579f7acdc
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-12 15:54:12 -0300

    chore: update header component css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,17 +6,17 @@ import { HeaderTitle } from "./HeaderTitle";
 export const Header = () => {
   return (
     <header
-      className="bg-cover bg-no-repeat text-whiteish w-auto"
+      className="bg-cover bg-no-repeat bg-fixed text-whiteish w-auto"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <Container>
         <HeaderTitle />
         <div className="mt-20">
-          <p className="text-7xl font-bold">
+          <p className="md:text-7xl text-3xl font-bold">
             Encontre livros <br /> que sejam a sua cara!
           </p>
           <p className="mt-3 mb-5 text-evergreen-light text-xl">Lorem Ipsum</p>
-          <Button title="Pesquisar livros"/>
+          <Button title="Pesquisar livros" onClick={() => console.log("")} />
         </div>
       </Container>
     </header>
