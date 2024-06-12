9e7454b1b8caff8e2a6c930207e08b957a185890
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-06-12 15:07:30 -0300

    Add card component (#10)

---
+++ src/components/Card/Card.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Card } from "./Card";
+
+const meta: Meta<typeof Card> = { component: Card };
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const Default: Story = { render: Card };
