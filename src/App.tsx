d3ba1790ff7319c4edf379ca82ac386d74b6a560
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-06-17 13:04:23 -0300

    Add books context (#19)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { BooksProvider } from "./contexts/BooksContext";
 import { AppRoutes } from "./routes";
 
 const App = () => {
-  return <AppRoutes />;
+  return (
+    <BooksProvider>
+      <AppRoutes />
+    </BooksProvider>
+  );
 };
 
 export default App;
